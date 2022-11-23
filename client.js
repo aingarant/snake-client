c9df89ff419d9ce767e6f1df327b95fdb394b312
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-23 10:39:24 -0500

    Added AT initials

--- client.js
+++ client.js
@@ -1,22 +1,23 @@
 const net = require("net");
 
 const connect = function () {
-  const conn = net.createConnection({
+  const client = net.createConnection({
     host: '192.168.50.101',
     port: 50541
   });
 
-  conn.setEncoding("utf8");
+  client.setEncoding("utf8");
 
-  conn.on('connect', () => {
+  client.on('connect', () => {
     console.log('we are good to go.. connected to server.');
+    client.write("Name: AT");
   });
 
-  conn.on('data', (data) => {
+  client.on('data', (data) => {
     console.log(data.toString());
   });
 
-  return conn;
+  return client;
 };
 
 module.exports = { connect };
